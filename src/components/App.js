2cd0bafdcd432b67e1924329a9c0a4ab86b22fff
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-07-08 18:10:43 -0300

    Implement App object

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, {Fragment} from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Math-magicians calculator
+        </p>
+      </header>
+      <>
+        < Display />
+        < ButtonPanel />
+      </>
+    </div>
+  );
+}
+
+export default App;
