0ce89ab2c498c0ca0a967c2aa766159cd53253b4
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-07-08 21:33:14 -0300

    Apply changes asked by the TSE

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,18 @@ import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Math-magicians calculator
-        </p>
-      </header>
-      <>
-        <Display />
-        <ButtonPanel />
-      </>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>
+        Math-magicians calculator
+      </p>
+    </header>
+    <>
+      <Display result="0" />
+      <ButtonPanel />
+    </>
+  </div>
+);
 
 export default App;
