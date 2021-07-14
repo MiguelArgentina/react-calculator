63da3e8902d9d5cb479353129aff10d6f29fc8d3
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-07-14 17:00:47 -0300

    Implement operate module

---
+++ src/logic/operate.js
@@ -0,0 +1,34 @@
+import Big from 'big';
+
+export const operate = (numberOne, numberTwo, operation) => {
+
+  let bigNumuberOne;
+  let bigNumberTwo;
+  if (numberOne) {
+    bigNumuberOne = Big(numberOne);
+  }
+  if (numberTwo) {
+    bigNumberTwo = Big(numberTwo);
+  }
+  try {
+    switch (operation) {
+      case '+/-':
+        return bigNumuberOne.times(-1)
+      case '+':
+        return bigNumuberOne.plus(bigNumberTwo)
+      case '-':
+        return bigNumuberOne.minus(bigNumberTwo)
+      case 'X':
+        return bigNumuberOne.times(bigNumberTwo)
+      case '÷':
+        return bigNumuberOne.div(bigNumberTwo)
+      case '%':
+        return bigNumuberOne.div(100)
+      default:
+        return '0';
+    }
+  } catch (error) {
+    return 'Error'
+  }
+
+}
