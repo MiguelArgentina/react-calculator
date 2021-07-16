e1624cb93882666bc7851df74a1c76244bc5447c
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-07-16 13:15:46 -0300

    Implement event handling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 
 const Button = (props) => {
-  const { name } = props;
+  const { name, clickHandler } = props;
   return (
-    <button type="button" className="btn btn-default">
+    <button type="button" className="btn btn-default" onClick={() => clickHandler(name)}>
       { name }
     </button>
   );
@@ -11,6 +11,7 @@ const Button = (props) => {
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired,
 };
 
 export default Button;
