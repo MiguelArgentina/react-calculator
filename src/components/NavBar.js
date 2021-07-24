efe02666fb0cae4a5418bb818e9c21e870b36919
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-07-24 18:55:45 -0300

    Add NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => (
+  <>
+    <h1>Math Magicians</h1>
+    <div>
+      <a href="#0">Home</a>
+      <a href="#0">Calculator</a>
+      <a href="#0">Quote</a>
+    </div>
+  </>
+);
+export default NavBar;
