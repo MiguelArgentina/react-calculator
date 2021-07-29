32dde62631368782b2584cdb9ba772287c1138eb
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-07-29 12:48:01 -0300

    Correct typo that made snapshot test fail

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const NotFound = () => (
   <>
     <div className="text-start home-para quote-para">
-      The page you are looking for does not exit.
+      The page you are looking for does not exist.
     </div>
     <p>
       <Link to="/" className="back-home-link">Back to home</Link>
